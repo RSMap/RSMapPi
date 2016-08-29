99fd7f3f6f00b8deb85e297c473e989393428314
José Manuel Luque Burgos <luqueburgosjm@gmail.com>
2016-08-29 23:15:47 +0200

    fail

--- src/iot/python_call_java/out/production/HelloWorld/caller.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import subprocess
-
-p = subprocess.Popen(["java", "Main"], stdin=subprocess.PIPE)
-p.stdin.write("First line\r\n")
-p.stdin.write("Second line\r\n")
-p.stdin.write("x\r\n")
--- src/iot/python_call_java/src/caller.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-p = subprocess.Popen(["java", "Main"], stdin=subprocess.PIPE)
-b = bytes("First line\r\n", 'utf-8')
-p.stdin.write(3)
-#p.stdin.write("Second line\r\n")
-p.stdin.write("x\r\n")
